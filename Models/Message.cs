f34f764dd797eee3844a765c656d8d99ae17705c
Ignat Bojinov <91905588+Ignat40@users.noreply.github.com>
2022-04-03 19:43:11 +0300

    Add files via upload

---
+++ Models/Message.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Message
+    {
+
+        public int Id { get; set; }
+        [Required] 
+        public string UserName { get; set; }
+        public string Text { get; set; }
+        [Required]
+        public DateTime When { get; set; }
+
+        public string UserID { get; set; }
+        public virtual AppUser Sender { get; set; }
+
+    }
+}
